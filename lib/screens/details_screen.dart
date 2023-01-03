312951f2bc8e20c5b3c0953c366bcfd6a6d06e48
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-01-03 00:16:50 -0600

    custom app bar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,11 +3,49 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
+  //  TODO: Cambiar luego por una instancia de movie
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DetailsScreen'),
+    //  Leer argumentos
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [_CustomAppBar()],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          alignment: Alignment.bottomCenter,
+          width: double.infinity,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage(
+              'https://images.nintendolife.com/b4fb0f19f7ab7/twilight-princess.large.jpg'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
